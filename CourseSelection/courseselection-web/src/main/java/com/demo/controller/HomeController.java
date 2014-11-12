75518e546d81ab82ff3e2abf565348486beb0257
xianfuzheng <zhengxianfu@gmail.com>
2014-11-12 13:28:03 +1100

    Add spring dependency
    POM ,WEB.XML
    with a simple controller

---
+++ CourseSelection/courseselection-web/src/main/java/com/demo/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value="/greeting.html",method=RequestMethod.GET)
+	public @ResponseBody String  greeting(){
+		
+		return "hello";
+	}
+	
+	
+}
